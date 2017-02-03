5b958da7dea06be6f61881ff45d9c7fff553fdc5
Primoz Cvenkelj <primoz.cvenkelj@gmail.com>
2017-02-03 10:23:50 +0100

    Randomly picing a color out of the color array

--- main.js
+++ main.js
@@ -8,9 +8,10 @@ var colors = [
 ];
 
 var squares = document.querySelectorAll('.square');
-var pickedColor = colors[3];
+var pickedColor = randomColor();
 var rgbDisplay = document.querySelector('#rgbDisplay');
 var message = document.querySelector('#message');
+var heading = document.querySelector('h1');
 
 rgbDisplay.textContent = pickedColor;
 
@@ -20,6 +21,7 @@ for(var i = 0; i < squares.length; i++){
     //adding listeners to squares
     squares[i].addEventListener('click', function(){
        var clickedColor = this.style.background;
+       // won
        if(clickedColor === pickedColor){
            message.textContent = 'Hooray!!!';
 
@@ -27,6 +29,8 @@ for(var i = 0; i < squares.length; i++){
                squares[i].style.background = pickedColor;
            }
 
+           heading.style.background = pickedColor;
+       //  try again
        } else {
            this.style.background = '#232323';
            message.textContent = 'Quess Again';
@@ -34,3 +38,8 @@ for(var i = 0; i < squares.length; i++){
     });
 }
 
+//picking up a random color
+function randomColor(){
+    var random = Math.floor(Math.random() * colors.length);
+    return colors[random];
+}
