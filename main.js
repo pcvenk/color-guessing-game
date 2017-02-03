763b7f5541532bc1236b4f62c6ff76ff998200e2
Primoz Cvenkelj <primoz.cvenkelj@gmail.com>
2017-02-03 09:54:30 +0100

    Adding event listeners to squares and fading out false attempts

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
+var colors = [
+  'rgb(255, 0, 0)',
+  'rgb(255, 255, 0)',
+  'rgb(255, 0, 255)',
+  'rgb(0, 255, 255)',
+  'rgb(0, 255, 0)',
+  'rgb(0, 0, 255)'
+];
+
+var squares = document.querySelectorAll('.square');
+var pickedColor = colors[3];
+
+var rgbDisplay = document.querySelector('#rgbDisplay');
+rgbDisplay.textContent = pickedColor;
+
+for(var i = 0; i < squares.length; i++){
+    //adding colors to squares
+    squares[i].style.background = colors[i];
+    //adding listeners to squares
+    squares[i].addEventListener('click', function(){
+       var clickedColor = this.style.background;
+       if(clickedColor === pickedColor){
+           console.log('You quessed it right');
+       } else {
+           console.log('Wrong');
+       }
+    });
+}
 
